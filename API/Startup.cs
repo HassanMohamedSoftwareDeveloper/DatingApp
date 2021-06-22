590fbf33b7388ef13a8b78372f070a48226c63ba
Hassan AbdelFattah <hmohammed.c@tetco.sa>
2021-06-22 15:42:11 +0200

    start angular client app

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,10 +32,15 @@ namespace API
             });
 
             services.AddControllers();
+
+           
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+
+             services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,7 +56,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(policy=>policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
