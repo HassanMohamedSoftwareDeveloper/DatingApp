fd4ee3b87789f69755e3d6639c09dab2e2507a00
Hassan AbdelFattah <hmohammed.c@tetco.sa>
2021-08-18 20:56:38 +0200

    Pre switch to Postgres database

--- API/Startup.cs
+++ API/Startup.cs
@@ -47,11 +47,16 @@ namespace API
             app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("https://localhost:4200"));
             app.UseAuthentication();
             app.UseAuthorization();
+            
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<PresenceHub>("hubs/presence");
                 endpoints.MapHub<MessageHub>("hubs/message");
+                endpoints.MapFallbackToController("Index","Fallback");
             });
         }
     }
